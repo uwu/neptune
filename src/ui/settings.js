2b91141cb2a705bb234af19bdde4e50ead6ce101
toonlink <toonlink@riseup.net>
2023-07-14 19:46:35 -0400

    make settings a little clearer

--- src/ui/settings.js
+++ src/ui/settings.js
@@ -61,7 +61,7 @@ const tabs = [
           <${TextInput} value=${pluginToImport} onEnter=${() => {
             installPluginFromURL(pluginToImport());
             pluginToImport("");
-          }} placeholder="https://example.com" />
+          }} placeholder="Import from URL" />
           <!-- This button will be used for importing from local files. -->
           <button onClick=${importLocalPlugin} class="neptune-round-button">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path><polyline points="17 8 12 3 7 8"></polyline><line x1="12" y1="3" x2="12" y2="15"></line></svg>
@@ -100,7 +100,7 @@ function PluginCard({ id, plugin }) {
               </svg>
             </button>
           </div>
-          <div>
+          <div class="padding-top: 10px; padding-bottom: 10px">
             <div>
               <span class="neptune-plugin-title">${plugin.manifest.name}</span>
               <span> by </span>
