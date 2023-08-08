ce8b48670b36c521f736ae5538aa475a8f688a31
toonlink <toonlink@riseup.net>
2023-08-08 19:36:16 -0400

    fix card padding

--- src/ui/pluginsTab.js
+++ src/ui/pluginsTab.js
@@ -80,7 +80,7 @@ function PluginCard({ plugin }) {
               <path stroke-linecap="round" stroke-linejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
             </svg>
           </button>
-          <div class="padding-top: 10px; padding-bottom: 10px">
+          <div style="padding-top: 10px;padding-bottom: 10px">
             <div>
               <span class="neptune-plugin-title">${plugin.manifest.name}</span>
               <span> by </span>
