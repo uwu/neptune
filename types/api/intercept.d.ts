cda0283c9d2050d659cee95a178bb785f94224dd
relative <hi@relative.im>
2023-09-11 16:24:10 -0400

    fix intercept type definition

--- types/api/intercept.d.ts
+++ types/api/intercept.d.ts
@@ -6,25 +6,31 @@ export type ActionType = keyof ActionTypes;
 export type PayloadActionTypeTuple<AT extends ActionType> = [ActionTypes[AT], AT];
 export type CallbackFunction<ForActionTypes extends ActionType> =
   /**
+   * @returns `true` to cancel dispatch
    * @returns anything else to continue
-   * @returns `false` to cancel dispatch
    */
-  <AT extends ForActionTypes>([payload, at]: PayloadActionTypeTuple<AT>) => false | unknown;
+  <AT extends ForActionTypes>([payload, at]: PayloadActionTypeTuple<AT>) => true | void;
 
 /**
  * intercept redux events
+ *
+ * return `true` from callback function to cancel the dispatch
  */
 export function intercept<AT extends ActionType>(
   type: AT,
   cb: CallbackFunction<AT>,
   once?: boolean,
 ): UninterceptFunction;
-// /**
-//  * dont use this signature
-//  * @deprecated
-//  */
-// export function intercept<AT extends ActionType>(
-//   types: AT[],
-//   cb: CallbackFunction<AT>,
-//   once = false
-// ): UninterceptFunction
+
+/**
+ * if using this signature you will have to manually cast action payloads
+ * to their proper types because narrowing generic types isn't
+ * possible yet in TypeScript
+ *
+ * return `true` from callback function to cancel the dispatch
+ */
+export function intercept<AT extends ActionType>(
+  types: AT[],
+  cb: CallbackFunction<AT>,
+  once?: boolean,
+): UninterceptFunction;
