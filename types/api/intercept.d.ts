d29c103f06c375441697f775394df83a6df3f0db
Inrixia <Inrixia@gmail.com>
2024-10-17 22:11:09 +1300

    Allow intercept callback to return Promise<void>

--- types/api/intercept.d.ts
+++ types/api/intercept.d.ts
@@ -9,7 +9,10 @@ export type CallbackFunction<ForActionTypes extends ActionType> =
    * @returns `true` to cancel dispatch
    * @returns anything else to continue
    */
-  <AT extends ForActionTypes>([payload, at]: PayloadActionTypeTuple<AT>) => true | void;
+  <AT extends ForActionTypes>([payload, at]: PayloadActionTypeTuple<AT>) =>
+    | true
+    | void
+    | Promise<void>;
 
 /**
  * intercept redux events
